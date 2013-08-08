da57432faa6115d6d8a085858a1d2e896d960dbe
lkdjiin <xavier.nayrac@gmail.com>
2013-08-08 13:27:15 +0200

    Document the Compiler class

--- lib/naam/compiler.rb
+++ lib/naam/compiler.rb
@@ -1,9 +1,18 @@
 module Naam
+
+  # Public: Here we transform a list of lexical units in a PIR source_lines
+  # code. At least this is the goal.
+  #
+  # The logic of this class follow the grammar of Naam (see the filename
+  # grammar.md).
   class Compiler
 
+    # Public: Compile lexical units from a Naam program in a PIR
+    # program.
+    #
     # units - Array of LexicalUnits
     #
-    # Returns ?
+    # Returns nothing.
     def compile units
       @units = units
       program
@@ -11,6 +20,9 @@ module Naam
 
     private
 
+    # This is the entry rule.
+    #
+    # Returns nothing.
     def program
       while @units.size > 0
         case @units.first.type
@@ -21,6 +33,9 @@ module Naam
       end
     end
 
+    # Returns nothing.
+    # Raises an error if the next units doesn't form an Naam's
+    #   instruction.
     def instruction
       case @units.first.type
       when :keyword then print_statement
@@ -30,17 +45,20 @@ module Naam
       end
     end
 
+    # Returns nothing.
     def print_statement
       accept(:keyword, 'print')
       accept_series(:word, :paro, :int, :parc, :eol)
     end
 
+    # Returns nothing.
     def function_def
       accept_series(:word, :paro, :word, :parc, :affect, :eol)
       if_clause while if_clause?
       else_clause
     end
 
+    # Returns nothing.
     def if_clause
       accept(:int)
       accept(:keyword, 'if')
@@ -48,16 +66,27 @@ module Naam
       accept(:eol)
     end
 
+    # Returns nothing.
     def else_clause
       accept(:int)
       accept(:keyword, 'else')
       accept(:eol)
     end
 
+    # Returns nothing.
     def test
       accept_series(:word, :op, :int)
     end
 
+    # Accepts the next lexical unit, given type match the unit type.
+    # If value is given, it has to match the unit value.
+    #
+    # type  - Expected Symbol type of the next unit.
+    # value - Expected String value of the next unit. Default is the
+    #         empty string, meaning the value doesn't matter.
+    #
+    # Returns nothing.
+    # Raises an error if type (and maybe value) doesn't match.
     def accept(type, value = '')
       unit = @units.slice!(0)
       raise Error unless unit.type == type
@@ -66,10 +95,17 @@ module Naam
       end
     end
 
+    # A shortcut method to #accept. Usefull when we have several calls
+    # to #accept. Calls #accept for each of the symbol in args.
+    #
+    # args - A series of Symbol.
+    #
+    # Returns nothing.
     def accept_series(*args)
       args.each {|arg| accept(arg) }
     end
 
+    # Returns Boolean true if the next units form an if clause.
     def if_clause?
       @units[1].type == :keyword && @units[1].value == 'if'
     end
